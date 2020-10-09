c8d910fef287a2bf43f9bc069d67cb3740d122be
ktalens <ktal66@yahoo.com>
2020-10-08 20:13:24 -0700

    images will generate when hitting submit button but will have a class of hidden and a display set to none. clicking the GO button after generating the hidden images will remove the hidden class from the images one at a time

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
-const requestUrl = 'https://www.reddit.com/search.json?q=kittens'
+const requestUrl = 'https://www.reddit.com/search.json?q='
 
-const addImages = (searchItem)=> {
-    //create a new image, set the source, append to ul
+const addImages = (searchItem,index)=> {
+    //create a new image, set the source, append to slideDisplay div
     let imageSlide = document.createElement('img');
     imageSlide.setAttribute('src',searchItem);
-    imageSlide.style.height = '200px';
-    slideShow.appendChild(imageSlide)
+    imageSlide.classList.add('slide');
+    imageSlide.classList.add('hidden');
+    imageSlide.id =index;
+    slideDisplay.appendChild(imageSlide);
 }
 
 const fetchSlides = () => {
-    fetch(requestUrl) //pending, resolved or rejected, then move onto the next...
+    fetch(requestUrl+input.value) //pending, resolved or rejected, then move onto the next...
         .then((responseData)=>{
             return responseData.json()
         })  
@@ -18,18 +20,37 @@ const fetchSlides = () => {
             let item = jsonData.data.children
             item.forEach((child,index)=>{
                 //console.log(index,child.data.url,child.data.thumbnail);
-                addImages(child.data.thumbnail);
+                addImages(child.data.thumbnail,index);
             })
         })
         .catch((error)=>{
             console.log("oh no, theres been an error",error)
         })
 }
+var currentSlide = 0
+
+const showSlideshow =()=> {
+let slides = document.querySelectorAll('.slide')
+slides[currentSlide].classList.remove('hidden');
+console.log(currentSlide);
+currentSlide += 1
+}
+
+
+
+
 
 document.addEventListener('DOMContentLoaded',() => {
     form.addEventListener('submit',(e)=>{
         e.preventDefault();
         console.log('user input is: '+input.value)
         fetchSlides();
+    document.getElementById('GO').addEventListener('click',
+        function(e) {
+            e.preventDefault();
+            console.log('click');
+            showSlideshow();
+        })
     })
+    
 })
